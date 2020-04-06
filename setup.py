9cda9145edd03bfc928d7d32ea71449c98a2570f
Kevin Froman <beardog@mailbox.org>
2020-04-06 03:24:44 -0500

    added long desc to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'Readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='streamedrequests',
-      version='1.0.2',
+      version='1.0.3',
       description='Library for streaming http get or post request\'s responses',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Kevin Froman',
       author_email='beardog@mailbox.org',
       url='https://github.com/beardog108/StreamedRequests',
